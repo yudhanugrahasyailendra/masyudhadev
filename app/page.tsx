9a9139efe085a4db677a545912482fb5c216aba6
yudhanugrahasyailendra <sylndrdev@gmail.com>
2026-09-05 15:34:15 +0800

    fix: update CV download link

--- app/page.tsx
+++ app/page.tsx
@@ -102,7 +102,7 @@ export default function HomePage() {
                 </div>
               </div>
               <div style={{ marginTop: '32px', paddingLeft: '26px' }}>
-                <Link className="btn-circuit" href="https://drive.google.com/file/d/1pLRblOp9IdN3ySlCYM0jrUUQXGFmvp_W/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                <Link className="btn-circuit" href="https://drive.google.com/file/d/1Ec2d0SJOPnxQ-dAmns2sdWiZill3mtyW/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                   <span>Download CV ↓</span>
                 </Link>
               </div>
